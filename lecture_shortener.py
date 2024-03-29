5d5abf72032e77c70696c24f6a411816a230a9e3
Jack Gawlik <jrgawlik50@gmail.com>
2024-03-29 18:29:33 -0400

    funny commit

--- lecture_shortener.py
+++ lecture_shortener.py
@@ -4,6 +4,7 @@ def main():
     print("Jack wrote this.")
     print("Zach wrote this.")
     print("Deets is causing problems")
+    print("deleting system 32")
 
 if __name__ == "__main__":
     main()

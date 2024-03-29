a672c8e84972bccd6025dfd37cc6eaed825baa65
Jack Gawlik <jrgawlik50@gmail.com>
2024-03-29 18:38:15 -0400

    new

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # LectureShortener
 For GT1201, Takes in a lecture video and reduces it into a more consumable format
 
+- git add .
+- git commit -m "message"
+- git push
+- git pull
+
 ## Requirements
 
 - calculate the talking speed of the video
--- lecture_shortener.py
+++ lecture_shortener.py
@@ -5,6 +5,13 @@ def main():
     print("Zach wrote this.")
     print("Deets is causing problems")
     print("deleting system 32")
+m = 0
+tot = 0
+while m <= 29:
+    tot = tot + 1
+    m = m + 1
+print("m")
+
 
 if __name__ == "__main__":
     main()
